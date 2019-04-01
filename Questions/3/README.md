ba1f462f68acdd4d5f19f5fd8d9d2406fe89055e
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-03-31 23:49:23 -0300

    Add question 3

---
+++ Questions/3/README.md
@@ -0,0 +1,4 @@
+# Questão 3
+
+- Abrir uma imagem colorida em RGB, visualizar e salvar cada um dos canais separadamente.
+Obs: Busquem compreender o que significa cada um dos canais.
---
+++ Questions/3/answer.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Read a rgb image
+image = cv2.imread('image.jpg')
+
+blue_channel, green_channel, red_channel = cv2.split(image)
+
+# Show the channels individually
+cv2.imshow('Blue Channel', blue_channel)
+cv2.imshow('Green Channel', green_channel)
+cv2.imshow('Red Channel', red_channel)
+
+cv2.waitKey(0)
+
+# Save the loaded image
+cv2.imwrite('blue_channel.jpg', blue_channel)
+cv2.imwrite('green_channel.jpg', green_channel)
+cv2.imwrite('red_channel.jpg', red_channel)
