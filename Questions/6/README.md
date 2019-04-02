1c7befb30433c16dc18d507c7a4064ff4ab39513
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-01 21:50:44 -0300

    Add question 6

---
+++ Questions/6/README.md
@@ -0,0 +1,5 @@
+# Questão 6
+
+- Abrir uma imagem colorida, transformar em tom de cinza, visualizar imagem de entrada. Apliquem os filtros passa alta 
+de canny (cv_canny), visualizem os resultados e salvem. Obs: busquem compreender os resultados do filtro.
+
---
+++ Questions/6/answer.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Read a rgb image
+image = cv2.imread('image.jpg')
+
+# Transform to grayscale
+grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply Canny filter (change the inferior and superior limit and see the difference)
+canny_image = cv2.Canny(grayscale_image, 80, 180)
+
+# Show the HSV image and its channels
+cv2.imshow('Canny filter result', canny_image)
+
+cv2.waitKey(0)
+
+# Save the results
+cv2.imwrite('canny_filter_result.jpg', canny_image)
