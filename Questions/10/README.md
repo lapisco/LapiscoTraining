dc9cca462f5ca1b40a19cdfa599bd199ab130bc5
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-01 22:50:37 -0300

    Add question 10

---
+++ Questions/10/README.md
@@ -0,0 +1,6 @@
+# Questão 10
+
+- Abrir uma imagem colorida, transformar em tom de cinza, visualizar imagem de entrada. Criem uma matriz de forma
+estática com as mesmas dimensões da imagem de entrada (vejam nas propriedades da imagem no Windows), peguem cada
+um dos pixels da imagem e coloquem na matriz que criaram. Apliquem uma limiarização fazendo uma varredura na matriz.
+Imprimam esta matriz em um arquivo de texto (*.txt) do mesmo modo que ela está alocada.
---
+++ Questions/10/answer.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+# Read a rgb image
+image = cv2.imread('image.jpg')
+
+# Transform to grayscale
+grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Show the input image
+cv2.imshow('Input grayscale image', grayscale_image)
+cv2.waitKey(0)
+
+# Get the rows and columns of the image
+rows, cols = grayscale_image.shape[:2]
+
+# Create a matrix of zeros
+threshold_matrix = np.zeros((rows, cols), dtype=np.uint8)
+
+for row in range(rows):
+    for col in range(cols):
+        threshold_matrix[row, col] = grayscale_image[row, col]
+
+# Save all pixels in a txt file
+with open('result.txt', 'w') as infile:
+    for row in range(rows):
+        for col in range(cols):
+            # Define the limits of the threshold
+            if threshold_matrix[row, col] < 127:
+                threshold_matrix[row, col] = 0
+            else:
+                threshold_matrix[row, col] = 255
+
+            infile.write(str(threshold_matrix[row, col]) + ' ')
+        infile.write('\n')
