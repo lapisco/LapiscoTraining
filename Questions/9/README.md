461436a88c6f5b8a6eb53dd00f7711e1e4197cd0
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-01 22:30:12 -0300

    Add question 9

---
+++ Questions/9/README.md
@@ -0,0 +1,6 @@
+# Questão 9
+
+-Abrir uma imagem colorida, transformar em tom de cinza, visualizar imagem de entrada. Criem uma matriz de forma
+estática com as mesmas dimensões da imagem de entrada (vejam nas propriedades da imagem no Windows), peguem cada
+um dos pixels da imagem e coloquem na matriz que criaram. Imprimam esta matriz em um arquivo de texto (*.txt) do
+mesmo modo que ela está alocada.
---
+++ Questions/9/answer.py
@@ -0,0 +1,24 @@
+import cv2
+
+# Read a rgb image
+image = cv2.imread('image.jpg')
+
+# Transform to grayscale
+grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Show the input image
+cv2.imshow('Input grayscale image', grayscale_image)
+cv2.waitKey(0)
+
+# Get the rows and columns of the image
+rows, cols = grayscale_image.shape[:2]
+
+# Save all pixels in a txt file
+with open('result.txt', 'w') as infile:
+    for row in range(rows):
+        for col in range(cols):
+            infile.write(str(grayscale_image[row, col]) + ' ')
+        infile.write('\n')
+
+
+
