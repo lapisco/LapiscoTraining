9949ab07342cb4112c148ba0c33ee75db1b1d64c
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-08 23:01:58 -0300

    Correct questions 35 and 36 to show results iteratively

--- Questions/35/answer.py
+++ Questions/35/answer.py
@@ -7,21 +7,22 @@ image = cv2.imread('image.jpg')
 # Transform to grayscale
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# Show the input image
+cv2.imshow('Input grayscale image', grayscale_image)
+
 # Apply the threshold
 ret, threshold_image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+# Show the result of the threshold
+cv2.imshow('Threshold image', threshold_image)
 
 # Create the structuring element
 kernel = np.ones((5, 5), np.uint8)
 
 # Apply the dilation
-dilation = cv2.dilate(threshold_image, kernel, iterations=5)
+for i in range(7):
+    dilation = cv2.dilate(threshold_image, kernel, iterations=i)
 
-# Show the input image
-cv2.imshow('Input grayscale image', grayscale_image)
-
-# Show the result of the threshold
-cv2.imshow('Threshold image', threshold_image)
-
-# Show the result of the dilation
-cv2.imshow('Dilated image', dilation)
-cv2.waitKey(0)
+    # Show the result of the dilation
+    cv2.imshow('Dilated image', dilation)
+    cv2.waitKey(1000)
--- Questions/36/answer.py
+++ Questions/36/answer.py
@@ -7,21 +7,21 @@ image = cv2.imread('image.jpg')
 # Transform to grayscale
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# Show the input image
+cv2.imshow('Input grayscale image', grayscale_image)
+
 # Apply the threshold
 ret, threshold_image = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+# Show the result of the threshold
+cv2.imshow('Threshold image', threshold_image)
 
 # Create the structuring element
 kernel = np.ones((5, 5), np.uint8)
 
 # Apply the erosion
-erosion = cv2.erode(threshold_image, kernel, iterations=5)
-
-# Show the input image
-cv2.imshow('Input grayscale image', grayscale_image)
-
-# Show the result of the threshold
-cv2.imshow('Threshold image', threshold_image)
-
-# Show the result of the erosion
-cv2.imshow('Dilated image', erosion)
-cv2.waitKey(0)
+for i in range(7):
+    erosion = cv2.erode(threshold_image, kernel, iterations=i)
+    # Show the result of the erosion
+    cv2.imshow('Dilated image', erosion)
+    cv2.waitKey(1000)
