5538a92081ce54cf3f5800ba34ac00cc23ba784f
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-12 19:39:10 -0300

    Add question 43

---
+++ Questions/43/README.md
@@ -0,0 +1,4 @@
+# Questão 43
+
+- Após fazer a questão 42, apresentem cada objeto na imagem, pintando o mesmo ou apresentando quadrados ao seus redor,
+ou ambos.
--- Questions/43/answer.py
+++ Questions/43/answer.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+import random
 
 # Read a rgb image
 image = cv2.imread('image.jpg')
@@ -8,9 +10,6 @@ grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Apply the Canny filter
 canny_image = cv2.Canny(grayscale_image, 80, 180)
-
-# Create a blob detector
-# detector = cv2.SimpleBlobDetector_create()
 
 # Define the parameters
 params = cv2.SimpleBlobDetector_Params()
@@ -43,6 +42,35 @@ blobs = detector.detect(canny_image)
 
 # Print how many objects are in the image
 print(len(blobs))
+
+rows, cols = image.shape[:2]
+
+# Draw blobs
+for k in blobs:
+
+    # Get the coordinates of up_left and bottom_right
+    x_up_left = int(k.pt[0] - k.size)
+    y_up_left = int(k.pt[1] - k.size)
+
+    x_bottom_right = int(k.pt[0] + k.size)
+    y_bottom_right = int(k.pt[1] + k.size)
+
+    # Verify if this coordinates are inside the limits of the image, correct if it is not inside the limits
+    if x_up_left < 0:
+        x_up_left = 0
+    if y_up_left < 0:
+        y_up_left = 0
+
+    if x_bottom_right > cols:
+        x_bottom_right = cols
+    if y_bottom_right > rows:
+        y_bottom_right = rows
+
+    # Draw the rectangle
+    cv2.rectangle(image, (x_up_left + 15, y_up_left + 15), (x_bottom_right - 15, y_bottom_right - 15), (255, 0, 0), 2)
+
+# Show the result
+cv2.imshow('Result', image)
 
 # Show the input image
 cv2.imshow('Input grayscale image', grayscale_image)
