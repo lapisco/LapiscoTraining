318b19b17c621ef47030f9458c05bdad61135b53
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-18 19:29:56 -0300

    Fix an error in question 51

--- Questions/51/answer.py
+++ Questions/51/answer.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 
 # Read the rgb images
 image = cv2.imread('image.jpg')
@@ -10,12 +8,18 @@ logo = cv2.imread('logo.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 logo = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
 
-# Create a SIFT detector
-orb = cv2.ORB_create()
+# Create a SIFT detector (Obs.: in order to use sift descriptor you will have to install opencv-contrib
+# from scratch. An alternative to that is to use opencv implementation called orb, it has almost the
+# same result. To use orb, comment the lines with sift and uncomment the lines with orb.)
+sift = cv2.xfeatures2d.SIFT_create()
+# orb = cv2.ORB_create()
 
 # Find the keypoints and descriptors with SIFT
-keypoints_1, descriptors_1 = orb.detectAndCompute(image, None)
-keypoints_2, descriptors_2 = orb.detectAndCompute(logo, None)
+keypoints_1, descriptors_1 = sift.detectAndCompute(image, None)
+keypoints_2, descriptors_2 = sift.detectAndCompute(logo, None)
+
+# keypoints_1, descriptors_1 = orb.detectAndCompute(image, None)
+# keypoints_2, descriptors_2 = orb.detectAndCompute(logo, None)
 
 # Create a BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
