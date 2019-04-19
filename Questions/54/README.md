fcd308c952577bf561ba858018efe67d83aefdd1
FabioXimenes <fabio.ximenes@lapisco.ifce.edu.br>
2019-04-19 10:33:41 -0300

    Add question 54

---
+++ Questions/54/README.md
@@ -0,0 +1,5 @@
+# Questão 54
+
+- Implementar o método de classificação k-médias (K-means em inglês) usando os métodos Hold out e Leave One Out. Tudo
+deve ser feito utilizando a estrutura Mat da OpenCv. Deve ser implementado o KNN com o K igual a 1, 3 e 7. Obs: Não usar
+as classes de Machine Learning da OpenCv.
---
+++ Questions/54/answer.py
@@ -0,0 +1,200 @@
+import numpy as np
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
+def hold_out(df, train_size, shuffle=True):
+
+    # Shuffle the dataframe if the shuffle is set to true
+    if shuffle:
+        df = df.sample(frac=1).reset_index(drop=True)
+
+    # Convert the rows of the dataframe into a list of lists
+    data = []
+    for row in df.iterrows():
+        index, values = row
+        data.append(values.tolist())
+
+    # Split the data into train and test
+    X_train = data[:int(train_size*len(data))]
+    X_test = data[int(train_size*len(data)):]
+
+    # Get the correspondent labels to each feature vector
+    y_train = [int(x[-1]) for x in X_train]
+    y_test = [int(x[-1]) for x in X_test]
+
+    # Remove the labels from the train and test vectors
+    X_train = [x[:-1] for x in X_train]
+    X_test = [x[:-1] for x in X_test]
+
+    return X_train, X_test, y_train, y_test
+
+
+def leave_one_out(df, shuffle=True):
+
+    # Shuffle the dataframe if the shuffle is set to true
+    if shuffle:
+        df = df.sample(frac=1).reset_index(drop=True)
+
+    # Convert the rows of the dataframe into a list of lists
+    data = []
+    for row in df.iterrows():
+        index, values = row
+        data.append(values.tolist())
+
+    # Create a list of lists, in which each iteration of leave one out will be stored
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+
+    for i in range(len(data)):
+        train = data.copy()
+        train.remove(data[i])
+
+        test = data[i]
+
+        # Get the correspondent labels to each feature vector
+        y_train.append([int(x[-1]) for x in train])
+        y_test.append(int(test[-1]))
+
+        # Remove the labels from the train and test vectors
+        X_train.append([x[:-1] for x in train])
+        X_test.append(test[:-1])
+
+    return X_train, X_test, y_train, y_test
+
+
+def read_data(file):
+    # Load the features of a file in a dataframe
+    return pd.read_csv(file, sep=',', header=None)
+
+
+def euclidean_dist(x1, x2):
+    dist = 0.0
+    for x, y in zip(x1, x2):
+        dist += pow(float(x) - float(y), 2)
+    dist = math.sqrt(dist)
+
+    return dist
+
+class KMeans:
+
+    def __init__(self, k=3, tolerance=0.0001, max_iterations=500):
+        self.k = k
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
+        self.centroids = {}
+
+    def fit(self, data):
+        # Initialize the centroids, the first 'k' elements in the dataset will be our initial centroids
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+
+        # Begin iterations
+        for i in range(self.max_iterations):
+            self.classes = {}
+            for i in range(self.k):
+                self.classes[i] = []
+
+            # Find the distance between the point and cluster; choose the nearest centroid
+            for features in data:
+                distances = [np.linalg.norm(np.array(features) - np.array(self.centroids[centroid])) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classes[classification].append(features)
+
+            previous = dict(self.centroids)
+
+            # Average the cluster datapoints to re-calculate the centroids
+            for classification in self.classes:
+                self.centroids[classification] = np.average(self.classes[classification], axis=0)
+
+            isOptimal = True
+
+            for centroid in self.centroids:
+                original_centroid = previous[centroid]
+
+                current = self.centroids[centroid]
+
+                if np.sum((current - original_centroid)/original_centroid * 100) > self.tolerance:
+                    isOptimal = False
+
+            # break out of the main loop if the results are optimal, ie. the centroids don't change their positions
+            # much(more than our tolerance)
+            if isOptimal:
+                break
+
+
+    def predict(self, data):
+        distances = [np.linalg.norm(data - self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+
+
+if __name__ == '__main__':
+
+    # Read the file with the features to classify
+    filename = 'features.txt'
+    features = read_data(filename)
+
+    # Split the database using hold out
+    X_train, X_test, y_train, y_test = hold_out(features, train_size=0.9)
+
+    # Apply k-means (you can change the number of clusters that you want to find, obs.: if you put a number of cluster
+    # greater than the number of classes, the algorithm will return predictions of classes that does not exist in the
+    # database.)
+    km = KMeans(k=3, max_iterations=10000)
+    km.fit(X_train)
+
+    predictions = []
+    for test in X_test:
+        predictions.append(km.predict(test))
+
+    # Calculates the accuracy using hold out
+    count = 0
+    for x, y in zip(y_test, predictions):
+        if x == y:
+            count += 1
+
+    accuracy = count/len(y_test)
+    print('Accuracy using hold out: {:.4f}'.format(accuracy))
+
+    # If you want to plot yours clusters, uncomment the following lines
+    # # Plotting starts here
+    # colors = 10 * ["r", "g", "c", "b", "k"]
+    #
+    # for centroid in km.centroids:
+    #     plt.scatter(km.centroids[centroid][0], km.centroids[centroid][1], s=130, marker="x")
+    #
+    # for classification in km.classes:
+    #     color = colors[classification]
+    #     for feature in km.classes[classification]:
+    #         plt.scatter(feature[0], feature[1], color=color, s=30)
+    #
+    # plt.show()
+
+    # Split the database using leave one out
+    X_train, X_test, y_train, y_test = leave_one_out(features)
+
+    # Apply knn (you can change the number of neighbors)
+
+    count = 0
+    for train_set, test_set, label_train, label_test in zip(X_train, X_test, y_train, y_test):
+
+        # Apply k-means (you can change the number of clusters that you want to find, obs.: if you put a number of cluster
+        # greater than the number of classes, the algorithm will return predictions of classes that does not exist in the
+        # database.)
+        km = KMeans(k=3, max_iterations=10000)
+        km.fit(train_set)
+
+        predictions = []
+
+        predict = km.predict(test_set)
+
+        if predict == label_test:
+            count += 1
+
+    accuracy = count / len(y_test)
+    print('Accuracy using leave one out: {:.4f}'.format(accuracy))
